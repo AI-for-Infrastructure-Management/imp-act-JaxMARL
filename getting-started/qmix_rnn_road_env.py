851118a12f25099db83283084a199231f7f51575
Daniel Hettegger <daniel.hettegger@tum.de>
2025-04-18 19:57:47 +0200

    Remove redundant config print

--- getting-started/qmix_rnn_road_env.py
+++ getting-started/qmix_rnn_road_env.py
@@ -892,8 +892,6 @@ def tune(default_config):
 @hydra.main(version_base=None, config_path="./config", config_name="qmix_rnn_road_env")
 def main(config):
     config = OmegaConf.to_container(config)
-    print("Config:\n", OmegaConf.to_yaml(config))
-
 
     if config.get("DOUBLE_PRECISION_MODE", False):
         jax.config.update("jax_enable_x64", True)
